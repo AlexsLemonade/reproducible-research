dd7e29bc572c59ff791537388e03ad7593ab3eb8
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-05-25 17:02:11 -0400

    for loop in script

--- workshop_materials/run_analysis.sh
+++ workshop_materials/run_analysis.sh
@@ -7,16 +7,23 @@ set -euo pipefail
 DATA_DIR=../data/processed/maf_files/
 RESULT_DIR=analysis/
 
-# Define input file
-MAF_FILE=Medulloblastoma.maf.tsv.gz
 
-# Define output file(s)
-RESULT_FILE=Medulloblastoma-gene_counts.tsv
+# Run the code over each of the following MAF names using a for loop
+for NAME in Ependymoma Ganglioglioma LGAT Medulloblastoma; do
 
-# Run the script to process the MAF file and export a table of gene counts
-Rscript 01_count-gene-mutations.R \ 
-  --maf ${DATA_DIR}/${MAF_FILE} \
-  --outfile ${RESULT_DIR}/${RESULT_FILE}
+    # Define input file
+    MAF_FILE=$NAME.maf.tsv.gz
+
+    # Define output file(s)
+    RESULT_FILE=$NAME-gene_counts.tsv
+
+    # Run the script to process the MAF file and export a table of gene counts
+    Rscript 01_count-gene-mutations.R \ 
+      --maf ${DATA_DIR}/${MAF_FILE} \
+      --outfile ${RESULT_DIR}/${RESULT_FILE}
   
   
-Rscript -e "rmarkdown::render('02_plot-gene-mutations.Rmd')"
+    # Run the notebook to visualize the results (or whatever it will do)
+    Rscript -e "rmarkdown::render('02_SOMETHING.Rmd')"
+
+done
