4934a9eaeda22cb2b18a92493f87f836352d9473
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-06-01 11:39:32 -0400

    Debugged scripts _confirmed_

--- instructor_notes/novariables_download-fastq.sh
+++ instructor_notes/novariables_download-fastq.sh
@@ -7,16 +7,16 @@ set -euo pipefail
 # Print an indicator:
 echo "Obtaining NC16_S1_L004_R1_001.fastq.gz"
 
-# Curl the file (using one of several approaches)
-curl -O https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/NC16_S1_L004_R1_001.fastq.gz
+# Curl the file (using one of several approaches, scroll down for more)
+curl -O https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/NC16_S1_L004_R1_001.fastq.gz
+
 
 # Explore: how many lines are in the file?
 echo "The number of lines in NC16_S1_L004_R1_001.fastq.gz is:"
 gunzip -c NC16_S1_L004_R1_001.fastq.gz | wc -l
   
 # Move the file to its destination directory
-mv NC16_S1_L004_R1_001.fastq.gz ../data/raw/fastq/SRP255885/NC16_S1_L004_R1_001.fastq.gz
-
+mv NC16_S1_L004_R1_001.fastq.gz ../data/raw/fastq/SRP255885/
 
 
 ##### Process the R2 file #####
@@ -25,14 +25,14 @@ mv NC16_S1_L004_R1_001.fastq.gz ../data/raw/fastq/SRP255885/NC16_S1_L004_R1_001.
 echo "Obtaining NC16_S1_L004_R2_001.fastq.gz"
 
 # Curl the file (using one of several approaches)
-curl -O https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/NC16_S1_L004_R2_001.fastq.gz
+curl -O https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/NC16_S1_L004_R2_001.fastq.gz
 
 # Explore: how many lines are in the file?
 echo "The number of lines in NC16_S1_L004_R2_001.fastq.gz is:"
 gunzip -c NC16_S1_L004_R2_001.fastq.gz | wc -l
   
 # Move the file to its destination directory
-mv NC16_S1_L004_R2_001.fastq.gz ../data/raw/fastq/SRP255885/NC16_S1_L004_R2_001.fastq.gz
+mv NC16_S1_L004_R2_001.fastq.gz ../data/raw/fastq/SRP255885/
 
 
 
@@ -45,10 +45,9 @@ mv NC16_S1_L004_R2_001.fastq.gz ../data/raw/fastq/SRP255885/NC16_S1_L004_R2_001.
 ##### For any version, you can always add `-s` to keep it quiet!
 
 # Approach 2: Download directly to the location you want
-#curl https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/NC16_S1_L004_R2_001.fastq.gz > ../data/raw/fastq/SRP255885/NC16_S1_L004_R1_001.fastq.gz
+#curl https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/NC16_S1_L004_R2_001.fastq.gz > ../data/raw/fastq/SRP255885/NC16_S1_L004_R1_001.fastq.gz
 
 # Approach 3: Same as above, but without stdout
-#curl https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/NC16_S1_L004_R2_001.fastq.gz -o ../data/raw/fastq/SRP255885/NC16_S1_L004_R1_001.fastq.gz
+#curl https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/NC16_S1_L004_R2_001.fastq.gz -o ../data/raw/fastq/SRP255885/NC16_S1_L004_R1_001.fastq.gz
 ##############################################################
-
 
--- instructor_notes/variables_download-fastq.sh
+++ instructor_notes/variables_download-fastq.sh
@@ -7,10 +7,10 @@ STUDY_ID="SRP255885"
 # Define file names
 FASTQ_R1="NC16_S1_L004_R1_001.fastq.gz"
 FASTQ_R2="NC16_S1_L004_R2_001.fastq.gz"
-URL_PREFIX="https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889"
+FASTQ_URL="https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889"
 
 # Define and create destination directory for FASTQ files to live in
-FASTQ_DEST="../data/raw/fastq/$STUDY_ID"
+FASTQ_DEST="../data/raw/fastq/$STUDY_ID/"
 mkdir -p $FASTQ_DEST 
 
 ##### Process the R1 file #####
@@ -19,19 +19,18 @@ mkdir -p $FASTQ_DEST
 echo "Obtaining $FASTQ_R1"
 
 # Curl the file (using one of several approaches)
-curl -O "$FASTQ_URL/$FASTQ_R1" # this approach preserves the original internet file
-mv $FASTQ_R1 $FASTQ_DEST/$FASTQ_R1 # move to destination directory
+curl -O $FASTQ_URL/$FASTQ_R1 # this approach preserves the original internet file name
 
 # Explore: how many lines are in the file?
 echo "The number of lines in $FASTQ_R1 is:"
-gunzip -c "$FASTQ_R1" | wc -l
+gunzip -c $FASTQ_R1 | wc -l
 
 ## Note: a fancier option if instructors want to show is - 
-##  NLINES=`gunzip -c "$FASTQ_R1" | wc -l`
+##  NLINES=`gunzip -c $FASTQ_R1 | wc -l`
 ##  echo "The number of lines in $FASTQ_R1 is $NLINES"
   
 # Move the file to its destination directory
-mv $FASTQ_R1 $FASTQ_DEST/$FASTQ_R1 
+mv $FASTQ_R1 $FASTQ_DEST
 
 
 ##### Process the R2 file #####
@@ -40,14 +39,13 @@ mv $FASTQ_R1 $FASTQ_DEST/$FASTQ_R1
 echo "Obtaining $FASTQ_R2"
 
 # Curl the file (using one of several approaches)
-curl -O "$FASTQ_URL/$FASTQ_R2" # this approach preserves the original internet file
-mv $FASTQ_R2 $FASTQ_DEST/$FASTQ_R2  # move to destination directory
+curl -O $FASTQ_URL/$FASTQ_R2 # this approach preserves the original internet file name
 
 # Explore: how many lines are in the file?
-gunzip -c "$FASTQ_DEST/$FASTQ_R2" | wc -l
+gunzip -c $FASTQ_R2 | wc -l
   
 # Move the file to its destination directory
-mv $FASTQ_R2 $FASTQ_DEST/$FASTQ_R2
+mv $FASTQ_R2 $FASTQ_DEST
 
 
 
@@ -59,10 +57,10 @@ mv $FASTQ_R2 $FASTQ_DEST/$FASTQ_R2
 ##### For any version, you can always add `-s` to keep it quiet!
 
 # Approach 2: Download directly to the location you want
-#curl "$FASTQ_URL/$FASTQ_R1" > "$FASTQ_DEST/$FASTQ_R1"
+#curl $FASTQ_URL/$FASTQ_R1 > $FASTQ_DEST/$FASTQ_R1
 
 # Approach 3: Same as above, but without stdout
-#curl "$FASTQ_URL/$FASTQ_R1" -o "$FASTQ_DEST/$FASTQ_R1"
+#curl $FASTQ_URL/$FASTQ_R1 -o $FASTQ_DEST/$FASTQ_R1
 ##############################################################
 
 
