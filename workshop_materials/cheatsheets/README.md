ea7e243fe890396dc56ba401a11c736d479b3401
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-06-02 14:53:50 -0400

    Run script to copy cheatsheets

--- workshop_materials/cheatsheets/README.md
+++ workshop_materials/cheatsheets/README.md
@@ -1,1 +1,6 @@
-`unix_quick_reference.md` was converted to PDF manually using the MacDown app (0.7.3) on MacOS. 
+`unix_quick_reference.md` is converted to PDF manually using the MacDown app (0.7.3) on MacOS. 
+
+The script `process_unix_cheatsheets.sh` does the following:
+
+- Knit the `unix_reference.Rmd` into html
+- Copy `unix_reference.html` and `unix_quick_reference.pdf` into ../../docs/
---
+++ workshop_materials/cheatsheets/process_unix_cheatsheets.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+# Note that the `unix_quick_reference.pdf` should be created manually by 
+# by exporting the `unix_quick_reference.md` as a PDF from the 
+# MacDown application on macOS _before_ running this script.
+
+# Directory where cheatsheets will be copied to
+DEST_DIR=../../docs/
+
+# Render the Rmarkdown
+Rscript -e "rmarkdown::render('unix_reference.Rmd')"
+
+# Copy files
+cp unix_reference.html ${DEST_DIR}
+cp unix_quick_reference.pdf ${DEST_DIR}
+
