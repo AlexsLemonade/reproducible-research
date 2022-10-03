48e86b2552a2fdb829119e01d1b70e1c0e90965e
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-10-03 11:19:44 -0400

    fixed

--- instructor_notes/fastq_subset/generate_fastq_subsets.sh
+++ instructor_notes/fastq_subset/generate_fastq_subsets.sh
@@ -8,16 +8,17 @@ set -euo pipefail
 # Function to curl, subset, and zip a FASTQ file
 # Provide a single argument: the FASTQ name, without `.gz` extension
 function subset_fastq() {
-    # Grab the file, unzip and take first 10000 records (40000 lines)
-    curl -s https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/$1.gz \
-    | gunzip \
+    # Grab the file
+    curl -sO https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/$1.gz
+
+    # Unzip and take first 10000 records (40000 lines)
+    gunzip -f $1.gz \
     | head -n 40000 \
     | gzip \
     > subset-${1}.gz
 
-
-    # Zip the new file in case instructors wish to do the whole `.gz` experience
-    gzip -c $1 > $1.gz
+    # Remove uncompressed file
+    rm $1
 }
 
 
