e92f1ca11e77e393367818f4a6ced2cd4bdd5a38
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-10-17 18:45:49 -0500

    subsets no longer empty

--- instructor_notes/fastq_subset/generate_fastq_subsets.sh
+++ instructor_notes/fastq_subset/generate_fastq_subsets.sh
@@ -12,13 +12,15 @@ function subset_fastq() {
     curl -sO https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/$1.gz
 
     # Unzip and take first 10000 records (40000 lines)
-    gunzip -f $1.gz \
+    gunzip -fc $1.gz \
     | head -n 40000 \
     | gzip \
     > subset-${1}.gz
 
-    # Remove uncompressed file
-    rm $1
+    # Remove original file. Note we keep the -c flag above and remove here,
+    #   since otherwise the final file is empty
+    rm $1.gz
+
 }
 
 
