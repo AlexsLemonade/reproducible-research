2713ed5cb59e370cd8682f7e8e79ecdc4f5a7c23
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2023-10-24 09:03:26 -0400

    Update novariables_download-fastq.sh

--- instructor_notes/novariables_download-fastq.sh
+++ instructor_notes/novariables_download-fastq.sh
@@ -5,34 +5,34 @@ set -euo pipefail
 ##### Process the R1 file #####
 
 # Print an indicator:
-echo "Obtaining NC16_S1_L004_R1_001.fastq.gz"
+echo "Obtaining SRR11518889_1.fastq.gz"
 
 # Curl the file (using one of several approaches, scroll down for more)
-curl -O https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/NC16_S1_L004_R1_001.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR115/089/SRR11518889/SRR11518889_1.fastq.gz
 
 
 # Explore: how many lines are in the file?
-echo "The number of lines in NC16_S1_L004_R1_001.fastq.gz is:"
-gunzip -c NC16_S1_L004_R1_001.fastq.gz | wc -l
+echo "The number of lines in SRR11518889_1.fastq.gz is:"
+gunzip -c SRR11518889_1.fastq.gz | wc -l
   
 # Move the file to its destination directory
-mv NC16_S1_L004_R1_001.fastq.gz ../data/raw/fastq/SRP255885/
+mv SRR11518889_1.fastq.gz ../data/raw/fastq/SRP255885/
 
 
 ##### Process the R2 file #####
 
 # Print an indicator:
-echo "Obtaining NC16_S1_L004_R2_001.fastq.gz"
+echo "Obtaining SRR11518889_2.fastq.gz"
 
 # Curl the file (using one of several approaches)
-curl -O https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/NC16_S1_L004_R2_001.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR115/089/SRR11518889/SRR11518889_2.fastq.gz
 
 # Explore: how many lines are in the file?
-echo "The number of lines in NC16_S1_L004_R2_001.fastq.gz is:"
-gunzip -c NC16_S1_L004_R2_001.fastq.gz | wc -l
+echo "The number of lines in SRR11518889_2.fastq.gz is:"
+gunzip -c SRR11518889_2.fastq.gz | wc -l
   
 # Move the file to its destination directory
-mv NC16_S1_L004_R2_001.fastq.gz ../data/raw/fastq/SRP255885/
+mv SRR11518889_2.fastq.gz ../data/raw/fastq/SRP255885/
 
 
 
@@ -45,8 +45,11 @@ mv NC16_S1_L004_R2_001.fastq.gz ../data/raw/fastq/SRP255885/
 ##### For any version, you can always add `-s` to keep it quiet!
 
 # Approach 2: Download directly to the location you want
-#curl https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/NC16_S1_L004_R2_001.fastq.gz > ../data/raw/fastq/SRP255885/NC16_S1_L004_R1_001.fastq.gz
+#curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR115/089/SRR11518889/SRR11518889_1.fastq.gz > ../data/raw/fastq/SRP255885/SRR11518889_1.fastq.g
 
+# Approach 3: Same as above, but without stdout
+#curl ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR115/089/SRR11518889/SRR11518889_1.fastq.gz -o ../data/raw/fastq/SRP255885/SRR11518889_1.fastq.g
+##############################################################
 # Approach 3: Same as above, but without stdout
 #curl https://sra-download.ncbi.nlm.nih.gov/traces/sra63/SRZ/011518/SRR11518889/NC16_S1_L004_R2_001.fastq.gz -o ../data/raw/fastq/SRP255885/NC16_S1_L004_R1_001.fastq.gz
 ##############################################################
