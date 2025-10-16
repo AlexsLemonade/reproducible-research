98a921c4252bad34f6504432aca749228e80dfc5
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-10-16 11:47:20 -0400

    add BASH_SOURCE line

--- instructor_notes/solutions/variables_download-fastq.sh
+++ instructor_notes/solutions/variables_download-fastq.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+# Set the working directory to the directory of this file
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 # Define study ID
 study_id="SRP255885"
 
@@ -11,7 +14,7 @@ fastq_url="ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR115/089/SRR11518889"
 
 # Define and create destination directory for FASTQ files to live in
 fastq_dest="../data/raw/fastq/$study_id/"
-mkdir -p $fastq_dest 
+mkdir -p $fastq_dest
 
 ##### Process the R1 file #####
 
@@ -25,7 +28,7 @@ curl -O $fastq_url/$fastq_r1 # this approach preserves the original internet fil
 echo "The number of lines in $fastq_r1 is:"
 gunzip -c $fastq_r1 | wc -l
 
-  
+
 # Move the file to its destination directory
 mv $fastq_r1 $fastq_dest
 
@@ -40,16 +43,16 @@ curl -O $fastq_url/$fastq_r2 # this approach preserves the original internet fil
 
 # Explore: how many lines are in the file?
 gunzip -c $fastq_r2 | wc -l
-  
+
 # Move the file to its destination directory
 mv $fastq_r2 $fastq_dest
 
 
 
 ############### Downloading files with curl ###################
-##### A few alternative approaches for downloading with curl are shown below, with R1 for example. 
+##### A few alternative approaches for downloading with curl are shown below, with R1 for example.
 ##### Depending on how the workshop is moving along, multiple versions can be optionally introduced.
-##### The versions shown below will place the downloaded file into the destination directory 
+##### The versions shown below will place the downloaded file into the destination directory
 #####    as part of the same step without needing a separate line to mv.
 ##### For any version, you can always add `-s` to keep it quiet!
 
